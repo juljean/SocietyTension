3c87397ccd8e9f6c102703a20121c1f6a8c35607
yuliia <yuliia.nad@g-experts.net>
2023-01-06 10:59:57 +0100

    fetch_comments is purified

--- fetch.py
+++ fetch.py
@@ -3,52 +3,15 @@ import json
 import googleapiclient.discovery
 import pandas as pd
 import configs
-
-#Ragulivna
-CHANNEL_ID = "UCf4A8MGpasfQTa28WncfreQ"
-VIDEO_ID = "ycV6bF_er3c"
-
-REMAIN_COLUMNS_CHANNEL = ["snippet.channelId", "id.videoId", "snippet.title", "snippet.publishTime"]
-
-FINAL_NAMES_COLUMNS_CHANNEL = ["channelId", "videoId", "title", "publishTime"]
-
-REMAIN_COLUMNS_VIDEO = ["snippet.topLevelComment.id", "snippet.topLevelComment.snippet.videoId",
-                  "snippet.topLevelComment.snippet.textOriginal",
-                  "snippet.topLevelComment.snippet.authorDisplayName",
-                  "snippet.topLevelComment.snippet.viewerRating,snippet.topLevelComment.snippet.likeCount",
-                  "snippet.topLevelComment.snippet.publishedAt"]
-FINAL_NAMES_COLUMNS_VIDEO = ['videoId','textOriginal', 'authorDisplayName', "likeCount", "publishedAt"]
+import constants
 
 
-def fetch_videos():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = configs.DEVELOPER_KEY
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-
-    request = youtube.search().list(
-        part="snippet",
-        channelId=CHANNEL_ID,
-        maxResults=100,
-        order="viewCount"
-    )
-    response = request.execute()
-    response = request.execute()["items"]
-    return response
-
-    # json_object = json.dumps(response, indent=4, ensure_ascii=False)
-
-    # with open("ChannelVideoData/" + str(CHANNEL_ID) + ".json", "w", encoding='utf-8') as outfile:
-    #     outfile.write(json_object)
-
-
-def fetch_comments():
+def fetch_comments(fetch_parameter):
+    """
+    Fetch data about videos/comments from YouTube API. Uncomment the block to save data in JSON
+    :param fetch_parameter: str, name of value which has to be fetched: videos/comments (branching may be continued)
+    :return: json_object with fetched data
+    """
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -56,22 +19,33 @@ def fetch_comments():
     api_service_name = "youtube"
     api_version = "v3"
 
+    # Address name of folder to be saved in
+    json_folder = ""
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=configs.DEVELOPER_KEY)
+    if fetch_parameter == "videos":
+        request = youtube.search().list(
+            part="snippet",
+            channelId=constants.CHANNEL_ID,
+            maxResults=100,
+            order="viewCount"
+        )
+        json_folder = "ChannelVideoData/"
 
-    request = youtube.commentThreads().list(
-        part="snippet",
-        maxResults=100,
-        videoId=VIDEO_ID
-    )
+    if fetch_parameter == "comments":
+        request = youtube.commentThreads().list(
+            part="snippet",
+            maxResults=100,
+            videoId=constants.VIDEO_ID
+        )
+        json_folder = "VideoCommentData/"
+
     response = request.execute()["items"]
-
-    # To save file in json
+    # # To save file in json
     # json_object = json.dumps(response, indent=4, ensure_ascii=False)
     #
-    # with open("VideoCommentData/" + str(VIDEO_ID) + ".json", "w", encoding='utf-8') as outfile:
+    # with open(json_folder + str(constants.VIDEO_ID) + ".json", "w", encoding='utf-8') as outfile:
     #     outfile.write(json_object)
-
     return response
 
 
@@ -85,9 +59,9 @@ def create_dataframe(json_object, folder_name, id, initial_columns, final_column
 
 
 def main():
-    json_object = fetch_videos()
-    # json_object = fetch_comments()
-    create_dataframe(json_object, "ChannelVideoData/", CHANNEL_ID, REMAIN_COLUMNS_CHANNEL, FINAL_NAMES_COLUMNS_CHANNEL)
+    # json_object = fetch_videos(CHANNEL_ID)
+    json_object = fetch_comments()
+    create_dataframe(json_object, "ChannelVideoData/", constants.VIDEO_ID, constants.REMAIN_COLUMNS_VIDEO, constants.FINAL_NAMES_COLUMNS_VIDEO)
 
 
 if __name__ == "__main__":
